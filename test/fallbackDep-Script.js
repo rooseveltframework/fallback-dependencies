a0292ac947de3ca455ac33dfbb83f0eebd41c64c
Railon Acosta <cagedtreestechnology@gmail.com>
2023-06-27 09:53:58 -0400

    Created a ./test/fallbackDep-script.js folder that test to make sure ./clones has three folders and that ./repos has three folders

--- test/fallbackDep-Script.js
+++ test/fallbackDep-Script.js
@@ -1,5 +1,4 @@
 /* eslint-env mocha */
-// wipe out test app after each test
 const assert = require('assert')
 const cleanupTestApp = require('./util/cleanupTestApp')
 const path = require('path')
@@ -24,8 +23,31 @@ describe('Testing script fallback-dep.js', function () {
     })
   })
 
-  it('Testing if node fallback-dep.js creates a clones and repos folder in the ./test folder', function () {
-  // test
+  it('Testing if node fallback-dep.js creates a clones and repos folder in the ./test folder', function (done) {
+  // test to see if ./test/clones exist
     assert(fs.existsSync('./test/clones') === true, './test/clones does not exist')
+    // test to see if ./test/repos exist
+    assert(fs.existsSync('./test/repos') === true, './test/repos does not exist')
+    done()
+  })
+
+  it('Testing if node fallback-dep.js creates three repos in the ./test/repos folder', function (done) {
+    // test to see if ./test/repos/repo1 exist
+    assert(fs.existsSync('./test/repos/repo1') === true, './test/repos/repo1 does not exist')
+    // test to see if ./test/repos/repo2 exist
+    assert(fs.existsSync('./test/repos/repo2') === true, './test/repos/repo2 does not exist')
+    // test to see if ./test/repos/repo3 exist
+    assert(fs.existsSync('./test/repos/repo3') === true, './test/repos/repo3 does not exist')
+    done()
+  })
+
+  it('Testing if node fallback-dep.js creates three clones of the repos in the ./test/clones folder', function (done) {
+    // test to see if ./test/clones/repo1 exist
+    assert(fs.existsSync('./test/clones/repo1') === true, './test/clones/repo1 does not exist')
+    // test to see if ./test/clones/repo2 exist
+    assert(fs.existsSync('./test/clones/repo2') === true, './test/clones/repo2 does not exist')
+    // test to see if ./test/clones/repo3 exist
+    assert(fs.existsSync('./test/clones/repo3') === true, './test/clones/repo3 does not exist')
+    done()
   })
 })
