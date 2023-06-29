f34ea452ef1e07797c191e4c2b4bf1a26c5ce0bd
Railon Acosta <cagedtreestechnology@gmail.com>
2023-06-29 11:55:28 -0400

    fallbackDep-Script.js passing test

--- test/fallbackDep-Script.js
+++ test/fallbackDep-Script.js
@@ -6,15 +6,19 @@ const cleanupTestApp = require('./util/cleanupTestApp')
 const path = require('path')
 const appDir = path.join(__dirname, 'app/paramFunctionTest')
 const fs = require('fs')
+const { execSync } = require('child_process')
 
 describe('Testing script fallbackDep.js', function () {
-  this.timeout(20000)
-
-  before(async () => {
-    const { execaNode } = await import('execa')
-    await execaNode`./test/fallbackDep.js`
+  // before(async () => {
+  //   const { execaNode } = await import('execa')
+  //   await execaNode`./fallbackDep.js`
+  //   this.timeout(20000)
+  // })
+  before(function (done) {
+    execSync('node ./../../fallbackDep.js')
+    this.timeout(20000)
+    done()
   })
-
   // delete the test app Directory and start with a clean state after each test
   afterEach(function (done) {
     cleanupTestApp(appDir, (err) => {
@@ -26,28 +30,28 @@ describe('Testing script fallbackDep.js', function () {
     })
   })
 
-  it.only('Testing if node fallback-dep.js creates a clones and repos folder in the ./test folder', async () => {
+  it('Testing if node fallbackDep.js creates a clones and repos folder in the ./test folder', function () {
     // test to see if ./test/clones exist
-    assert(fs.existsSync('./test/clones') === true, './test/clones does not exist')
+    assert(fs.existsSync(path.join(__dirname, '/clones')) === true, './test/clones does not exist')
     // test to see if ./test/repos exist
-    assert(fs.existsSync('./test/repos') === true, './test/repos does not exist')
+    assert(fs.existsSync(path.join(__dirname, '/repos')) === true, './test/repos does not exist')
   })
 
-  it.only('Testing if node fallback-dep.js creates three repos in the ./test/repos folder', function () {
+  it('Testing if node fallback-dep.js creates three repos in the ./test/repos folder', function () {
     // test to see if ./test/repos/repo1 exist
-    assert(fs.existsSync('./test/repos/repo1') === true, './test/repos/repo1 does not exist')
+    assert(fs.existsSync(path.join(__dirname, '/repos/repo1')) === true, './test/repos/repo1 does not exist')
     // test to see if ./test/repos/repo2 exist
-    assert(fs.existsSync('./test/repos/repo2') === true, './test/repos/repo2 does not exist')
+    assert(fs.existsSync(path.join(__dirname, '/repos/repo2')) === true, './test/repos/repo2 does not exist')
     // test to see if ./test/repos/repo3 exist
-    assert(fs.existsSync('./test/repos/repo3') === true, './test/repos/repo3 does not exist')
+    assert(fs.existsSync(path.join(__dirname, '/repos/repo3')) === true, './test/repos/repo3 does not exist')
   })
 
-  it.only('Testing if node fallback-dep.js creates three clones of the repos in the ./test/clones folder', function () {
+  it('Testing if node fallback-dep.js creates three clones of the repos in the ./test/clones folder', function () {
     // test to see if ./test/clones/repo1 exist
-    assert(fs.existsSync('./test/clones/repo1') === true, './test/clones/repo1 does not exist')
+    assert(fs.existsSync(path.join(__dirname, '/clones/repo1')) === true, './test/clones/repo1 does not exist')
     // test to see if ./test/clones/repo2 exist
-    assert(fs.existsSync('./test/clones/repo2') === true, './test/clones/repo2 does not exist')
+    assert(fs.existsSync(path.join(__dirname, '/clones/repo2')) === true, './test/clones/repo2 does not exist')
     // test to see if ./test/clones/repo3 exist
-    assert(fs.existsSync('./test/clones/repo3') === true, './test/clones/repo3 does not exist')
+    assert(fs.existsSync(path.join(__dirname, '/clones/repo3')) === true, './test/clones/repo3 does not exist')
   })
 })
